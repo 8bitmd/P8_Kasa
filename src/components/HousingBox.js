2ebe1bff6846f8d6e94f6cd6efaeb3b617f921f2
Bruno Tumedei <brunotumedei@gmail.com>
2024-01-10 17:55:26 +0100

    Add Link for each housing card

--- src/components/HousingBox.js
+++ src/components/HousingBox.js
@@ -1,14 +1,17 @@
 import "../styles/housing.css"
+import {Link} from "react-router-dom";
 const data = require("../datas/logements.json")
 
 export default function HousingBox() {
     return (
         <div className="housing">
             {data.map((housing) => (
-                <div className="housing_box" key={housing.id}>
-                    <img src={housing.cover}/>
-                    <p>{housing.title}</p>
-                </div>
+                <Link to={`/${housing.id}`}>
+                    <div className="housing_box" key={housing.id}>
+                        <img src={housing.cover}/>
+                        <p>{housing.title}</p>
+                    </div>
+                </Link>
             ))}
         </div>
     )

9fa91f1588072b13a9a16ca5ff7d0e45cdf1ace0
Bruno Tumedei <brunotumedei@gmail.com>
2024-01-16 23:55:49 +0100

    Style gallery

--- src/components/AboutElement.js
+++ src/components/AboutElement.js
@@ -2,16 +2,12 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import {faChevronUp} from "@fortawesome/free-solid-svg-icons";
 import "../styles/aboutelement.css"
 
-function expandCollapse() {
-
-}
-
 export const AboutElement = ({title, content}) => {
     return (
         <div className="element_container">
             <div className="element_header">
                 <h1>{title}</h1>
-                <FontAwesomeIcon onClick={expandCollapse} size="xl" icon={faChevronUp} style={{color: "#ffffff",}}/>
+                <FontAwesomeIcon size="xl" icon={faChevronUp} style={{color: "#ffffff",}}/>
             </div>
             <div className="element_content">
                 <p>{content}</p>
--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,5 @@
 import {useState} from "react";
+import "../styles/gallery.css"
 
 export const Gallery = ({photos}) => {
     const [index, setIndex] = useState(0)
@@ -12,22 +13,25 @@ export const Gallery = ({photos}) => {
 
     let currentPhoto = photos[index]
     return (
-        <>
-
-            <button onClick={previousPhoto}>
-                <svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path
-                        d="M47.0399 7.42497L39.9199 0.344971L0.359863 39.945L39.9599 79.545L47.0399 72.465L14.5199 39.945L47.0399 7.42497Z"
-                        fill="white"/>
-                </svg>
-            </button>
-            <button onClick={nextPhoto}>
-                <svg width="48" height="81" viewBox="0 0 48 81" fill="none" xmlns="http://www.w3.org/2000/svg">
-                    <path d="M0.959961 72.9877L8.03996 80.0677L47.64 40.4677L8.03996 0.867676L0.959961 7.94768L33.48 40.4677L0.959961 72.9877Z" fill="white"/>
-                </svg>
-            </button>
-            <img src={currentPhoto}/>
-            <p>{index + 1}/{photos.length}</p>
-        </>
+        <div className={"gallery_container"}>
+            <div className={"gallery"}>
+                <button className={"previous_photo"} onClick={previousPhoto}>
+                    <svg width="48" height="80" viewBox="0 0 48 80" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M47.0399 7.42497L39.9199 0.344971L0.359863 39.945L39.9599 79.545L47.0399 72.465L14.5199 39.945L47.0399 7.42497Z"
+                            fill="white"/>
+                    </svg>
+                </button>
+                <button className={"next_photo"} onClick={nextPhoto}>
+                    <svg width="48" height="81" viewBox="0 0 48 81" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path
+                            d="M0.959961 72.9877L8.03996 80.0677L47.64 40.4677L8.03996 0.867676L0.959961 7.94768L33.48 40.4677L0.959961 72.9877Z"
+                            fill="white"/>
+                    </svg>
+                </button>
+                <img className={"gallery_photo"} src={currentPhoto}/>
+                <p>{index + 1}/{photos.length}</p>
+            </div>
+        </div>
     )
 }
